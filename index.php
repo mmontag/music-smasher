accdfd2941b025f5a842cf3e1b77c827eb6f4b95
Matt Montag <montag@spotify.com>
2013-01-11 03:29:32 -0800

    Add embedded player frame!
    - Also remove some old fist smash markup

--- index.php
+++ index.php
@@ -35,37 +35,46 @@ Date: February 12, 2011
   <script type="text/javascript" src="<?= $app_path ?>/smasher.js"></script>
 </head>
 <body class="centered">
+<div class="container">
+  <table class="table"><tr><td class="search">
+    <form id="qform">
+      <h1 class="pagetitle"><a href="<?= $app_path ?>/">
+        Music&#8202;Smasher
+      </a></h1>
+      <div class="tagline">Find Any Song.</div>
+      <input id="q"/>
+      <!--
+      <div class="tip">
+        Hot Tip! Use Shift+Enter to search if you're feeling lucky.
+      </div>
+      -->
+      <div class="logos">
+        <img src="images/logos_white_retina.png">
+      </div>
+      <div class="details">
+        <a href="http://www.mattmontag.com/music/music-smasher-streaming-music-api-mashup">Feedback</a> - 
+        
+        <iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.musicsmasher.net%2F&amp;send=false&amp;layout=button_count&amp;width=85&amp;show_faces=false&amp;action=like&amp;colorscheme=dark&amp;font=lucida+grande&amp;height=21&amp;appId=132115193511995" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:85px; height:21px; vertical-align: bottom;" allowTransparency="true"></iframe>
 
-<table class="table"><tr><td class="search">
-  <form id="qform">
-    <h1 class="pagetitle"><a href="<?= $app_path ?>/">
-      Music&#8202;Smasher
-    </a></h1>
-    <div class="tagline">Find Any Song.</div>
-    <input id="q"/>
-    <!-- <input type="submit" class="Button" value="Smash"/> -->
-    <div class="logos">
-      <img src="images/logos_white_retina.png">
+        - <input type="checkbox" id="us" name="us" checked /> US Catalog Only
+      </div>
+    </form>
+  </td></tr></table>
+  <div class="content">
+    <div id="services"></div>
+
+    <div class="footer">
+      by <a href="http://www.mattmontag.com">Matt Montag</a>
     </div>
-    <div class="details">
-      <a href="http://www.mattmontag.com/music/music-smasher-streaming-music-api-mashup">Feedback</a> - 
-      
-      <iframe src="//www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.musicsmasher.net%2F&amp;send=false&amp;layout=button_count&amp;width=85&amp;show_faces=false&amp;action=like&amp;colorscheme=dark&amp;font=lucida+grande&amp;height=21&amp;appId=132115193511995" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:85px; height:21px; vertical-align: bottom;" allowTransparency="true"></iframe>
-
-      - <input type="checkbox" id="us" name="us" checked /> US Catalog Only
-    </div>
-  </form>
-</td></tr></table>
-<div class="content">
-  <div id="services"></div>
-
-  <div class="footer">
-    by <a href="http://www.mattmontag.com">Matt Montag</a>
+  </div>
+  <div class="playContainerSpacer"></div>
+  <div class="playContainer">
+    <iframe class="playFrame" name="playFrame"></iframe>
   </div>
 </div>
 <!-- Templates -->
 <script type="text/template" id="tpl-track">
-  <li><a href="{{ url }}" target="m">{% if (artist) { %}{{ artist }} - {% } %}{{ track }}</a>
+  <li><a href="{{ url }}" data-autoplayurl="{{ autoPlayUrl }}" target="m">{% if (artist) { %}{{ artist }} - {% } %}{{ track }}</a>
   <span class="album">{{ album }}</span></li>
 </script>
 <script type="text/template" id="tpl-service">
